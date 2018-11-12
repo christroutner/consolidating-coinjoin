d9a58bbba33511034d3fff68e5295e4955d2ff53
Chris Troutner <chris.troutner@gmail.com>
2018-11-11 18:00:42 -0800

    Added utility functions to swap out and delete active wallet

--- src/utils/check-balance.js
+++ src/utils/check-balance.js
@@ -4,16 +4,25 @@
 
 'use strict'
 
+const shelljs = require('shelljs')
+
+// Wallet functionality
+const CreateWallet = require('bch-cli-wallet/src/commands/create-wallet')
+
 const FILENAME = `${__dirname}/../../wallets/wallet.json`
+const ACTIVE_WALLET = `${__dirname}/../../wallets/active-wallet.json`
 const THRESHOLD = 0.1
 
 const util = require('util')
 util.inspect.defaultOptions = { depth: 1 }
 
 module.exports = {
-  checkBalance // Check the balance of the wallet.
+  checkBalance, // Check the balance of the wallet.
+  swapWallet,
+  deleteWallet
 }
 
+// Query the balance of the wallet and update the wallet file.
 async function checkBalance (walletInfo, BITBOX, updateBalance) {
   try {
     const newWalletInfo = await updateBalance.updateBalances(FILENAME, walletInfo, BITBOX)
@@ -34,4 +43,32 @@ async function checkBalance (walletInfo, BITBOX, updateBalance) {
     console.log(`Error in check-balance.js/checkBalance()`)
     throw err
   }
+}
+
+// Swaps out the existing wallet with a new wallet. Also increments the ROUND.
+async function swapWallet (BITBOX) {
+  // Swap out the existing wallet.
+  shelljs.mv(FILENAME, ACTIVE_WALLET)
+
+  // Update the ROUND.
+  process.env.ROUND = Number(process.env.ROUND) + 1
+
+  // Create a new wallet.
+  const createWallet = new CreateWallet()
+  let walletInfo
+  if (process.env.NETWORK === `testnet`) {
+    walletInfo = await createWallet.createWallet(FILENAME, BITBOX, true)
+  } else {
+    walletInfo = await createWallet.createWallet(FILENAME, BITBOX, false)
+  }
+
+  return walletInfo
+}
+
+// Delete the active wallet.
+function deleteWallet () {
+  // Delete the wallet.
+  shelljs.rm(ACTIVE_WALLET)
+
+  // TODO: Delete the entries in the DB
 }
--- test/a05-check-balance.js
+++ test/a05-check-balance.js
@@ -11,6 +11,7 @@ const utils = require('./utils')
 const assert = require('chai').assert
 const checkBalance = require('../src/utils/check-balance')
 const sinon = require('sinon')
+const shelljs = require('shelljs')
 
 const { bitboxMock } = require('./mocks/bitbox')
 const exampleWallet = require('./mocks/testwallet.json')
@@ -20,7 +21,7 @@ util.inspect.defaultOptions = { depth: 1 }
 
 // const LOCALHOST = 'http://localhost:5000'
 
-describe('Check Balance', () => {
+describe('Check Balance Utilities', () => {
   let mockedWallet
   let BITBOX
 
@@ -33,14 +34,36 @@ describe('Check Balance', () => {
     mockedWallet = Object.assign({}, exampleWallet) // Clone the testwallet
   })
 
-  it('should update the balance', async () => {
-    const updateBalance = {
-      updateBalances: sinon.stub().returns({ balance: 0.01 })
-    }
+  describe('Check Balance', () => {
+    it('should update the balance', async () => {
+      const updateBalance = {
+        updateBalances: sinon.stub().returns({ balance: 0.01 })
+      }
 
-    const balance = await checkBalance.checkBalance(mockedWallet, BITBOX, updateBalance)
-    // console.log(`balance: ${balance}`)
+      const balance = await checkBalance.checkBalance(mockedWallet, BITBOX, updateBalance)
+      // console.log(`balance: ${balance}`)
 
-    assert.equal(balance, 0.01)
+      assert.equal(balance, 0.01)
+    })
+  })
+
+  describe('Wallet Utils', () => {
+    it('should swap out the wallet file.', async () => {
+      await checkBalance.swapWallet(BITBOX)
+
+      const afterFileList = shelljs.ls(`${__dirname}/../wallets/`)
+
+      // Assert that the file exists in the directory.
+      assert.equal(afterFileList.indexOf(`active-wallet.json`) > -1, true)
+    })
+
+    it('should delete the active wallet file', async () => {
+      await checkBalance.deleteWallet()
+
+      const afterFileList = shelljs.ls(`${__dirname}/../wallets/`)
+
+      // Assert that the active wallet has been deleted.
+      assert.equal(afterFileList.indexOf(`active-wallet.json`) === -1, true)
+    })
   })
 })
