64e57fa76fc9209d948ac51e7fabbe698c76629a
Chris Troutner <chris.troutner@gmail.com>
2018-11-15 20:12:45 -0800

    fix(/address tests): Updated tests to reflect code changes

--- test/unit/a04-address.js
+++ test/unit/a04-address.js
@@ -23,7 +23,69 @@ describe('Address', () => {
   describe('POST /address', () => {
     let prevRound
 
-    it('should return input addresses', async () => {
+    it('should reject a non-array output', async () => {
+      const outAddrs = 'bchtest:qr05vk3f4d2efnajvz7ns7wlz52atf5vgyhj8ydgug'
+      const numInputs = 1
+      const amount = 1000
+
+      const options = {
+        method: 'POST',
+        uri: `${LOCALHOST}/address`,
+        resolveWithFullResponse: true,
+        json: true,
+        body: {
+          outAddrs,
+          numInputs,
+          amount
+        }
+      }
+
+      try {
+        await rp(options)
+        // console.log(`result.body: ${util.inspect(result.body)}`)
+
+        assert.equal(true, false, 'Unexpected result!')
+      } catch (err) {
+        // console.log(`err: `, err)
+        assert.equal(err.statusCode, 422, `Expect 422 error`)
+        assert.include(err.message, 'outputAddrs must be an array')
+      }
+    })
+
+    it('should reject a non-number amount', async () => {
+      const outAddrs = [
+        'bchtest:qr05vk3f4d2efnajvz7ns7wlz52atf5vgyhj8ydgug',
+        'bchtest:qpddsjjrrvsh5gu3eqxqrln4sgx58kaly576tc2xn9',
+        'bchtest:qpfmklaz53e23f04rnqkgl03x2hhgskkuukjfcea56'
+      ]
+      const numInputs = 2
+      const amount = 'abc'
+
+      const options = {
+        method: 'POST',
+        uri: `${LOCALHOST}/address`,
+        resolveWithFullResponse: true,
+        json: true,
+        body: {
+          outAddrs,
+          numInputs,
+          amount
+        }
+      }
+
+      try {
+        await rp(options)
+        // console.log(`result.body: ${util.inspect(result.body)}`)
+
+        assert.equal(true, false, 'Unexpected result!')
+      } catch (err) {
+        // console.log(`err: `, err)
+        assert.equal(err.statusCode, 422, `Expect 422 error`)
+        assert.include(err.message, 'amount needs to be a number')
+      }
+    })
+
+    it('should reject a non-integer amount', async () => {
       const outAddrs = [
         'bchtest:qr05vk3f4d2efnajvz7ns7wlz52atf5vgyhj8ydgug',
         'bchtest:qpddsjjrrvsh5gu3eqxqrln4sgx58kaly576tc2xn9',
@@ -31,6 +93,39 @@ describe('Address', () => {
       ]
       const numInputs = 2
       const amount = 0.0123
+
+      const options = {
+        method: 'POST',
+        uri: `${LOCALHOST}/address`,
+        resolveWithFullResponse: true,
+        json: true,
+        body: {
+          outAddrs,
+          numInputs,
+          amount
+        }
+      }
+
+      try {
+        await rp(options)
+        // console.log(`result.body: ${util.inspect(result.body)}`)
+
+        assert.equal(true, false, 'Unexpected result!')
+      } catch (err) {
+        // console.log(`err: `, err)
+        assert.equal(err.statusCode, 422, `Expect 422 error`)
+        assert.include(err.message, 'amount needs to be an integer (satoshis)')
+      }
+    })
+
+    it('should return input addresses', async () => {
+      const outAddrs = [
+        'bchtest:qr05vk3f4d2efnajvz7ns7wlz52atf5vgyhj8ydgug',
+        'bchtest:qpddsjjrrvsh5gu3eqxqrln4sgx58kaly576tc2xn9',
+        'bchtest:qpfmklaz53e23f04rnqkgl03x2hhgskkuukjfcea56'
+      ]
+      const numInputs = 2
+      const amount = 1230000
 
       const options = {
         method: 'POST',
@@ -64,7 +159,7 @@ describe('Address', () => {
 
       assert.equal(result.body.inputAddrs.length, 2)
       assert.equal(result.body.outputAddrs.length, 3)
-      assert.equal(result.body.satoshisReported, 0.0123)
+      assert.equal(result.body.satoshisReported, 1230000)
 
       prevRound = result.body.round
     })
@@ -76,7 +171,7 @@ describe('Address', () => {
         'bchtest:qpfmklaz53e23f04rnqkgl03x2hhgskkuukjfcea56'
       ]
       const numInputs = 2
-      const amount = 0.0123
+      const amount = 1230000
 
       // Increment the global ROUND env var.
       process.env.ROUND = prevRound + 1
