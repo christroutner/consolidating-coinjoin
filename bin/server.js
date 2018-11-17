c7dfb813a82e9eea3610ed9bbb84066d2691a720
Chris Troutner <chris.troutner@gmail.com>
2018-11-17 10:14:32 -0800

    Introducing Winston logging and logs directory

--- bin/server.js
+++ bin/server.js
@@ -9,6 +9,7 @@ const mount = require('koa-mount')
 const serve = require('koa-static')
 const cors = require('kcors')
 const checkBalance = require('../src/utils/check-balance')
+const winston = require('winston')
 
 // Wallet functionality
 const CreateWallet = require('bch-cli-wallet/src/commands/create-wallet')
@@ -61,6 +62,30 @@ async function startServer () {
     if (mongoose.connection.collections.hasOwnProperty(collection)) {
       mongoose.connection.collections[collection].deleteMany()
     }
+  }
+
+  // LOGGING
+
+  const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+      //
+      // - Write to all logs with level `info` and below to `combined.log`
+      // - Write all logs error (and below) to `error.log`.
+      //
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' })
+    ]
+  })
+  //
+  // If we're not in production then log to the `console` with the format:
+  // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+  //
+  if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple()
+    }))
   }
 
   // MIDDLEWARE START
